92daccfe23d224f9282d98e5d6f538ed718c63c3
wangxiaofei <2217079516@qq.com>
2020-04-02 22:49:26 +0800

    element-ui中el-form以及Notice的实现

---
+++ el-form/src/components/form/Index.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <KForm :model="model" :rules="rules" ref="loginForm">
+            <KFormItem label="用户名" prop="username">
+                <KInput v-model="model.username" placeholder="请输入用户名"></KInput>
+            </KFormItem>
+            <KFormItem label="密码" prop="password">
+                <KInput v-model="model.password" placeholder="请输入密码"></KInput>
+            </KFormItem>
+            <KFormItem>
+                <button @click="onClick">登录</button>
+            </KFormItem>
+        </KForm>
+    </div>
+</template>
+
+<script>
+    import KInput from '@/components/form/KInput.vue'
+    import KFormItem from '@/components/form/KFormItem.vue'
+    import KForm from '@/components/form/KForm.vue'
+    import Notice from '@/components/Notice.vue'
+    export default {
+        components: {
+            KInput,
+            KFormItem,
+            KForm
+        },
+        data() {
+            return {
+                rules: {
+                    username: [{ required: true, message: "请输⼊⽤户名" }],
+                    password: [{ required: true, message: "请输⼊密码" }]
+                },
+                model: {
+                    username: '',
+                    password: ''
+                }
+            };
+        },
+        methods: {
+            onClick() {
+                this.$refs.loginForm.validate(isValid => {
+                    if(isValid) {
+                        console.log()
+                    } else {
+                       this.$create(Notice, {
+                           title: '校验失败',
+                           message: '校验错误请重试'
+                       }).show()
+                    }
+                })
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ el-form/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import { create } from '@/utils/create.js'
+
+Vue.config.productionTip = false
+Vue.prototype.$create = create
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
